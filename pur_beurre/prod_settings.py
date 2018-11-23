54a65a1b638e2d288384d4078d7074b08f7bc4d5
horlas <aurelia.gourbere@gmail.com>
2018-11-23 14:01:44 +0100

    modification du prod.setting.py pour depliement sur serveur

--- pur_beurre/prod_settings.py
+++ pur_beurre/prod_settings.py
@@ -16,9 +16,8 @@ MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-DATABASES['default'] = db_from_env
+# used only for heroku deployment
+# db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#
+# DATABASES['default'] = db_from_env
 
