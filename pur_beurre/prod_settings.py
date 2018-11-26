5ae6fbc1320033849ec7e8c4dca31d50ef5a2665
horlas <aurelia.gourbere@gmail.com>
2018-11-26 10:51:01 +0100

    ajout utilisateur database

--- pur_beurre/prod_settings.py
+++ pur_beurre/prod_settings.py
@@ -1,14 +1,25 @@
 import dj_database_url
 from pur_beurre.settings import *
 
-
 DEBUG = False
 
 TEMPLATE_DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['purbeurre.cedrix.org', '*']
+ALLOWED_HOSTS = ['purbeurre.cedrix.org' , '*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quality',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST': '',
+        'PORT': '5432',
+    }
+}
 
 
 # MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
@@ -21,4 +32,4 @@ ALLOWED_HOSTS = ['purbeurre.cedrix.org', '*']
 #
 # DATABASES['default'] = db_from_env
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
