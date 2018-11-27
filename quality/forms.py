c11b11cb1bbe7f7ae1abe85d4fd2a2424765f0e4
horlas <aurelia.gourbere@gmail.com>
2018-11-27 15:15:50 +0100

    initialisation de sentry

--- quality/forms.py
+++ quality/forms.py
@@ -1,4 +1,3 @@
-from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from bootstrap_modal_forms.mixins import PopRequestMixin, CreateUpdateAjaxMixin
